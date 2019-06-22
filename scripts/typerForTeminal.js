70834a50e938461686ea9be022d22bf242a272f7
joshhauser <none>
2019-06-23 00:25:32 +0200

    reworkd style

--- scripts/typerForTeminal.js
+++ scripts/typerForTeminal.js
@@ -2,15 +2,9 @@ $(window).ready(function(){
     setTimeout(function(){
         var domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
         var typed = new Typed("#typed", {
-            /* strings: [
-            "^1000 ls ^500 <br>`test.txt<span style='padding-right: 20px'></span>helloWorld.txt<span style='padding-right: 20px'></span>index.php<br><br>" + domainName + "`"+
-            "^500 cat index.php ^500 <br>`&lt;?php<br><span style='padding-right: 15px'></span>echo \"Hello\";<br>?&gt;<br><br>" + domainName + "`"+
-            "^500 cd .. ^500 <br>`<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~/home</span><span style='color: white'>$</span>`"
-            ], */
-
             strings: [
               "^1000 cat presentation.txt"+
-              "^500 `<br><br>Prénom: Joshua<br>Nom: Hauser<br>Email: joshua.hauser@gmx.fr<br>Numéro: 06.51.02.60.66<br><br>" + domainName + "`"
+              "^500 `<br><br>Prénom: Joshua<br>Nom: Hauser<br>Email: <a href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a><br>Numéro: <a href='tel:+33651026066'>06.51.02.60.66</a><br><br>" + domainName + "`"
               ],
             typeSpeed: 40
         }); 
