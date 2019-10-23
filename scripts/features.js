5fb929478d52766495a4b76746c2734edb1b4735
joshhauser <none>
2019-10-23 15:19:15 +0200

    ;;

--- scripts/features.js
+++ scripts/features.js
@@ -5,4 +5,11 @@ function toggleMenu() {
   } else {
     x.style.display = "block";
   }
-} 
+}
+
+document.onclick = function(event){
+  if (event.target && ((event.target.id && event.target.id !== "switch") || (event.target.class && event.target.class !== "burger"))){
+    toggledMenu = document.getElementById("toggled-menu");
+    if(toggledMenu.style.display === "block") toggledMenu.style.display = "none";
+  }
+}
