8fb8410bcdac5b249f4df5b1f068acd6bfdc6bdd
Joshua HAUSER <joshua.hausser@ens.uvsq.fr>
2021-04-08 23:32:03 +0200

    Terminal
    Replaced textual introduction by a terminal with an imitation
    of 'neofetch' Linux package.
    Fake terminal is removed for windows under 950px width then it's
    replaced by the textual introduction.

--- scripts/custom_typed.js
+++ scripts/custom_typed.js
@@ -1,14 +1,33 @@
 $(window).ready(function(){
-  setTimeout(function(){
-    var domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
-    var typed = new Typed("#typed", {
-      strings: [
-        "^1000 cat presentation.txt"+
-        "^500 `<br><br>Prénom: Joshua<br>Nom: Hauser<br>Email: <a href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a><br>Numéro: <a href='tel:+33651026066'>06.51.02.60.66</a><br><br>" + domainName + "`"
-        ],
-      typeSpeed: 40
-    }); 
-  }, 1000);
+  var domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
+  var typed = new Typed("#typed", {
+    strings: [
+      "^1000 fetch introduction" +
+      "^500 `<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.##%&#%#*/.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Joshua HAUSER</span>" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,***///*(#/(/#(,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  -------------" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.*/,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;....*,/,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Téléphone : </span><a style='color: white' href='tel:+33651026066'>06.51.02.60.66</a>" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Email : </span><a style='color: white' href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a>" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,,.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Études : </span> ingénieur en informatique à l'ISTY" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;.*,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Permis : </span>B" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.*,..../(*.&nbsp;&nbsp;&nbsp;*/%&,*.*/,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Dernier diplôme : </span>DUT Informatique" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,(,&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,,,,&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....&nbsp;&nbsp;..*,.......&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...,..&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;.*..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....,,**/(((*,.&nbsp;&nbsp;.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,....,.......&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;.......&nbsp;.....,.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;...&nbsp;" +
+      "<br><br>" +
+      domainName + "`"
+
+      /*<a href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a><br>Numéro: <a href='tel:+33651026066'>06.51.02.60.66</a>*/
+    ],
+    typeSpeed: 40
+  }); 
+  
+
 
   var skills = new Typed("#typedSkills", {
     strings: [
