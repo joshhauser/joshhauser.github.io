68453b462ae8fab5d0e9e78ed067606ff40e7eaa
joshhauser <joshua.hauser@mx.fr>
2021-08-03 20:32:06 +0200

    implemented terminal typing triggering on scroll

--- scripts/custom_typed.js
+++ scripts/custom_typed.js
@@ -1,35 +1,5 @@
-$(window).ready(function(){
-  var domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
-  var typed = new Typed("#typed", {
-    strings: [
-      "^1000 fetch introduction" +
-      "^500 `<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.##%&#%#*/.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Joshua HAUSER</span>" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,***///*(#/(/#(,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  -------------" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.*/,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;....*,/,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Téléphone : </span><a style='color: white' href='tel:+33651026066'>06.51.02.60.66</a>" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Email : </span><a style='color: white' href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a>" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,,.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Études : </span> ingénieur en informatique à l'ISTY" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;.*,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Permis : </span>B" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.*,..../(*.&nbsp;&nbsp;&nbsp;*/%&,*.*/,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Dernier diplôme : </span>DUT Informatique" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,(,&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,,,,&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....&nbsp;&nbsp;..*,.......&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...,..&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;.*..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....,,**/(((*,.&nbsp;&nbsp;.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,....,.......&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
-      "&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;.......&nbsp;.....,.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;...&nbsp;" +
-      "<br><br>" +
-      domainName + "`"
-
-      /*<a href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a><br>Numéro: <a href='tel:+33651026066'>06.51.02.60.66</a>*/
-    ],
-    typeSpeed: 40
-  }); 
-  
-
-
-  var skills = new Typed("#typedSkills", {
+$(window).ready(() => {
+  let skills = new Typed("#typedSkills", {
     strings: [
       "^500 Python",
       "^500 Java",
@@ -42,4 +12,45 @@ $(window).ready(function(){
     shuffle: true,
     loop: true
   })
+
+
+  let introOffsetTop = document.getElementById("header").offsetTop;
+  let domainName;
+  let typed;
+
+  $(window).scroll(() => {
+    if (window.scrollY >= introOffsetTop && domainName === undefined && typed === undefined) {
+  
+      $(window).unbind("scroll");
+      domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
+      typed = new Typed("#typed", {
+        strings: [
+          "^1000 fetch introduction" +
+          "^500 `<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.##%&#%#*/.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Joshua HAUSER</span>" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,***///*(#/(/#(,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  -------------" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.*/,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;....*,/,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Téléphone : </span><a style='color: white' href='tel:+33651026066'>06.51.02.60.66</a>" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Email : </span><a style='color: white' href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a>" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,,.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Études : </span> ingénieur en informatique à l'ISTY" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;.*,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Permis : </span>B" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.*,..../(*.&nbsp;&nbsp;&nbsp;*/%&,*.*/,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class='introductionLabel'>Dernier diplôme : </span>DUT Informatique" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,(,&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,,,,&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....&nbsp;&nbsp;..*,.......&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...,..&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;.*..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.....,,**/(((*,.&nbsp;&nbsp;.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,....,.......&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<br>" +
+          "&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;.......&nbsp;.....,.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;...&nbsp;" +
+          "<br><br>" +
+          domainName + "`"
+  
+          /*<a href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a><br>Numéro: <a href='tel:+33651026066'>06.51.02.60.66</a>*/
+        ],
+        typeSpeed: 40
+      }); 
+    }
+  
+  });
 });
+
