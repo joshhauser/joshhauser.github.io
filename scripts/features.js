55c6b1780f0309661b708b560c164140e6e51fa4
joshhauser <joshua.hauser@gmx.fr>
2022-05-08 23:06:45 +0200

    Began new version of portfolio

--- scripts/features.js
+++ scripts/features.js
@@ -1,4 +1,4 @@
-function toggleMenu() {
+/* function toggleMenu() {
   var x = document.getElementById("toggled-menu");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -12,4 +12,22 @@ document.onclick = function(event){
     toggledMenu = document.getElementById("toggled-menu");
     if(toggledMenu.style.display === "block") toggledMenu.style.display = "none";
   }
+} */
+
+function configMenu() {
+  const menu = document.getElementById('menu');
+  const menuItems = menu.childNodes
+  for (let i = 0; i < menuItems.length; i++) {
+    console.log(menuItems)
+    menuItems[i].addEventListener('click', (e) => {
+      const activeItem = menu.getElementsByClassName('active')[0];
+      activeItem.classList.toggle('active');
+      console.log(e.target)
+      e.target.classList.toggle('active');
+    })
+  } 
+}
+
+window.onload = () => {
+  configMenu();
 }
