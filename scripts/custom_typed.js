5dbefd307d10796c5acdaaed4459e0dd531ad1de
Josh <joshua.hauser@gmx.fr>
2024-11-29 12:20:40 +0100

    Cleaned code + added custom CSS for PDF printing brower's feature

--- scripts/custom_typed.js
+++ scripts/custom_typed.js
@@ -1,7 +1,7 @@
 $(window).ready(function(){
   setTimeout(function(){
-    var domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
-    var typed = new Typed("#typed", {
+    let domainName = "<span style='color: #3fca78;'>user@network:</span><span style='color: #3f72ca'>~</span><span style='color: white'>$</span>"
+    new Typed("#typed", {
       strings: [
         "^1000 cat presentation.txt"+
         "^500 `<br><br>Prénom: Joshua<br>Nom: Hauser<br>Email: <a href='mailto:joshua.hauser@gmx.fr'>joshua.hauser@gmx.fr</a><br>Numéro: <a href='tel:+33651026066'>06.51.02.60.66</a><br><br>" + domainName + "`"
@@ -10,7 +10,7 @@ $(window).ready(function(){
     }); 
   }, 1000);
 
-  var skills = new Typed("#typedSkills", {
+  new Typed("#typedSkills", {
     strings: [
       "^500 Python",
       "^500 Java",
